15b47527b46dc0e5010edbef35a77d1df19e982c
ya-web <idir.alliche@gmail.com>
2025-03-04 09:09:05 +0100

    Refactor environment configuration in settings.py to improve .env loading and enhance security; streamline ALLOWED_HOSTS and CORS settings.

--- backend/ogtickets/settings.py
+++ backend/ogtickets/settings.py
@@ -4,33 +4,32 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Setting environment configuration
 ENV = os.getenv('ENV', 'dev')
 
-if ENV != 'prod' and ENV != 'production':
-    load_dotenv(os.path.join(BASE_DIR, '.env'))
+if ENV not in ['prod', 'production']:
+    ENV_FILEPATH = os.path.join(BASE_DIR, f'.env')
+    if os.path.exists(ENV_FILEPATH):
+        # Load environment variables from .env file
+        load_dotenv(ENV_FILEPATH)
+    else:
+        raise Exception(f"Missing .env file at {ENV_FILEPATH}")
 
     if ENV == 'test':
-        ENV_FILEPATH = os.path.join(BASE_DIR, '.env.test')
+        ENV_TEST_FILEPATH = os.path.join(BASE_DIR, '.env.test')
         # ".env.test" file is required for testing
-        if os.path.exists(ENV_FILEPATH):
-            load_dotenv(ENV_FILEPATH, override=True)
+        if os.path.exists(ENV_TEST_FILEPATH):
+            load_dotenv(ENV_TEST_FILEPATH, override=True)
         else: 
-            raise Exception(f"Missing .env.test file at {ENV_FILEPATH}")
+            raise Exception(f"Missing .env.test file at {ENV_TEST_FILEPATH}")
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG', True))
-
-ALLOWED_HOSTS = []
-
+DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 'yes']
+# Set ALLOWED_HOSTS from the environment variable; default to 'localhost,127.0.0.1,[::1]' to allow local IPv4 and IPv6.
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,[::1]').split(',')]
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -54,7 +53,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS : Transform ALLOWED_HOSTS to include "http" or "https" scheme
+scheme = "http"
+if ENV in ["prod","production"] :
+    scheme = "https"
+
+CORS_ALLOWED_ORIGINS = [f"{scheme}://{host}" for host in ALLOWED_HOSTS]
 
 ROOT_URLCONF = "ogtickets.urls"
 
@@ -77,7 +81,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "ogtickets.wsgi.application"
 
 # REST Framework settings
-
 REST_FRAMEWORK = {
     # Default settings (you can customize as needed)
     'DEFAULT_PERMISSION_CLASSES': [
@@ -85,9 +88,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-
 # Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -102,9 +103,7 @@ DATABASES = {
 if os.getenv('ENV', 'dev') in ['prod', 'production']:
     DATABASES['default']['OPTIONS']['sslmode'] = 'require'
 
-
 # Password validation
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -123,7 +122,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -135,10 +133,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "/static/"
+# Directory where collectstatic will gather all static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
