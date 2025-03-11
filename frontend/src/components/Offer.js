0a1b6fc5fa23e8b5ae9512b03a64e04f1c738662
ya-web <idir.alliche@gmail.com>
2025-03-11 20:43:48 +0100

    Add front end Offers feature with API integration and new Offer components

---
+++ frontend/src/components/Offer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Offer = ({ index, offer }) => {
+    const { name, description, price } = offer;
+
+    return (
+        <div key={index} className="offer-card" data-testid="offer">
+            <h3 data-testid="offer-heading">{name}</h3>
+            <p data-testid="offer-description">{description}</p>
+            <p data-testid="offer-price"><strong>Prix :</strong> {price}</p>
+            <button>Sélectionner l'offre {name}</button>
+        </div>
+    );
+};
+
+export default Offer;
