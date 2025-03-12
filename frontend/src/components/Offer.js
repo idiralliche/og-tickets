88789e3264493d30f9f52ee341d7267666b99e91
ya-web <idir.alliche@gmail.com>
2025-03-13 00:37:50 +0100

    Add flash effect to Offer button on click and implement useFlashOnButtonClick hook

--- frontend/src/components/Offer.js
+++ frontend/src/components/Offer.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import { NotificationContext } from '../context/NotificationContext';
+import { useFlashOnButtonClick } from '../hooks/useFlashOnButtonClick';
 
 const Offer = ({ offer }) => {
   const { name, description, price } = offer;
   const { addOffer } = useContext(CartContext);
   const { showNotification } = useContext(NotificationContext);
+  const [isFlashing, triggerFlash] = useFlashOnButtonClick(150);
 
-  // Handler to add the offer to the cart without navigation
+  // Handler to add the offer to the cart
   const handleAddToCart = () => {
+    triggerFlash();
     addOffer(offer);
     showNotification(` Une offre ${name} ajoutée à votre panier`, 'success');
   };
@@ -18,7 +21,10 @@ const Offer = ({ offer }) => {
       <h3 data-testid='offer-heading'>{name}</h3>
       <p data-testid='offer-description'>{description}</p>
       <p data-testid='offer-price'>{price} €</p>
-      <button className='button' onClick={handleAddToCart}>
+      <button
+        className={`button ${isFlashing ? 'clicked' : ''}`}
+        onClick={handleAddToCart}
+      >
         Ajouter au panier
       </button>
     </div>
