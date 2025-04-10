cfa9133d686f8691b8ebff90831800026c654198
ya-web <idir.alliche@gmail.com>
2025-04-10 10:38:53 +0200

    Refactor secret handling in start.sh for improved readability and efficiency

--- backend/start.sh
+++ backend/start.sh
@@ -3,27 +3,41 @@
 # Enable strict mode: exit on error
 set -e
 
+# Required secrets as space-separated list
+required_secrets="secret_key db_user db_password db_name db_host allowed_hosts sentry_dsn email_host email_port email_host_user email_host_password email_use_tls default_from_email cors_allowed_origins"
+
 # Function to check if all required secrets are present
 check_secrets() {
-  local missing_secrets=0
-  for secret in "$@"; do
+  missing_secrets=0
+  for secret in $1; do
     if [ ! -f "/run/secrets/$secret" ]; then
       echo "Erreur : Le secret '$secret' est manquant."
       missing_secrets=1
     fi
   done
-  if [ $missing_secrets -ne 0 ]; then
+  if [ "$missing_secrets" -ne 0 ]; then
     echo "Des secrets requis sont manquants. Arrêt du script."
     exit 1
   fi
 }
 
+# Check if all required secrets are present
+echo "Vérification de l'existence des secrets Docker..."
+check_secrets "$required_secrets"
+
+# Load secrets from /run/secrets/ and export them as environment variables
+for secret in $required_secrets; do
+  env_var=$(echo "$secret" | tr '[:lower:]' '[:upper:]')
+  value=$(cat "/run/secrets/$secret" | tr -d '\n')
+  export "$env_var=$value"
+done
+
 # Function to wait for the database to be ready
 wait_for_db() {
-  local db_host="$1"
-  local db_port="$2"
-  local max_wait_time=60
-  local elapsed=0
+  db_host="$1"
+  db_port="$2"
+  max_wait_time=60
+  elapsed=0
 
   if [ -z "$db_host" ] || [ -z "$db_port" ]; then
     echo "Erreur : DB_HOST ou DATABASE_PORT n'est pas défini."
@@ -43,6 +57,9 @@ wait_for_db() {
   done
   echo "La base de données est disponible !"
 }
+
+# Wait for the database to be ready
+wait_for_db "$DB_HOST" "${DATABASE_PORT:-5432}"
 
 # Function to apply migrations
 apply_migrations() {
@@ -64,27 +81,9 @@ apply_migrations() {
   rm -f /tmp/migrate_output.log
 }
 
-# List of required secrets
-required_secrets=(
-  secret_key db_user db_password db_name db_host allowed_hosts sentry_dsn
-  email_host email_port email_host_user email_host_password email_use_tls
-  default_from_email cors_allowed_origins
-)
-
-# Check if all required secrets are present
-echo "Vérification de l'existence des secrets Docker..."
-check_secrets "${required_secrets[@]}"
-
-# Load secrets from /run/secrets/ and export them as environment variables
-for secret in "${required_secrets[@]}"; do
-  export "$(echo "$secret" | tr '[:lower:]' '[:upper:]')"=$(cat "/run/secrets/$secret" | tr -d '\n')
-done
-
-# Wait for the database to be ready
-wait_for_db "$DB_HOST" "${DATABASE_PORT:-5432}"
-
 # Apply migrations
 apply_migrations
 
 # Launch Gunicorn to start the Django app
+echo "Démarrage de Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8000 --workers 1 ogtickets.wsgi:application
