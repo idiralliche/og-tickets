aaf2c0bf14d4e91b22d96031df7d4fa07adb38dc
ya-web <idir.alliche@gmail.com>
2025-04-21 01:56:56 +0200

    refactor: Remove collectstatic from Dockerfile and handle it in start.sh with error logging

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,13 +1,4 @@
 from django.apps import AppConfig
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.contrib.auth.models import AbstractUser
-from typing import TYPE_CHECKING, Any
-
-# Avoid circular imports
-if TYPE_CHECKING:
-    from django.http import HttpRequest
-
 
 class AccountsConfig(AppConfig):
     """
@@ -27,7 +18,7 @@ class AccountsConfig(AppConfig):
         # Connect signal handler
         signals.user_registered.connect(self._handle_user_registration)
 
-    def _handle_user_registration(self, user: AbstractUser, request: "HttpRequest", **kwargs: Any) -> None:
+    def _handle_user_registration(self, user, request, **kwargs):
         """Post-registration workflow"""
         from .utils import send_activation_email
 
