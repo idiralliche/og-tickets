9ef4f96ee2a418f148450c93c7314874b17fb311
ya-web <idir.alliche@gmail.com>
2025-05-22 00:04:09 +0200

    feat: Refactor CartPage to use InCartOffersForOlympicEvent component and improve cart management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import OlympicEventsPage from './pages/OlympicEventsPage';
-import OffersPage from './pages/OffersPage.js';
 import CartPage from './pages/CartPage';
 import AccountPage from './pages/AccountPage.js';
 import ActivationPage from './pages/ActivationPage';
@@ -10,58 +9,57 @@ import ForgotPasswordPage from './pages/ForgotPasswordPage.js';
 import ResetPasswordConfirmPage from './pages/ResetPasswordConfirmPage.js';
 import UnauthenticatedOnly from './wrappers/UnauthenticatedOnly';
 import MemberOnly from './wrappers/MemberOnly';
+import { useCartSyncOnLogin } from './hooks/useCartSyncOnLogin';
 import './main.css';
 
 const App = () => {
+  useCartSyncOnLogin();
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/epreuves' element={<OlympicEventsPage />} />
-        <Route path='/offres' element={<OffersPage />} />
-        <Route path='/panier' element={<CartPage />} />
-        <Route
-          path='/acces/ouverture'
-          element={
-            <UnauthenticatedOnly redirectTo='/'>
-              <ActivationPage />
-            </UnauthenticatedOnly>
-          }
-        />
-        <Route
-          path='/acces/break'
-          element={
-            <UnauthenticatedOnly redirectTo='/'>
-              <ForgotPasswordPage />
-            </UnauthenticatedOnly>
-          }
-        />
-        <Route
-          path='/acces/reprise'
-          element={
-            <UnauthenticatedOnly redirectTo='/'>
-              <ResetPasswordConfirmPage />
-            </UnauthenticatedOnly>
-          }
-        />
-        <Route
-          path='/loge'
-          element={
-            <MemberOnly redirectTo='/acces'>
-              <AccountPage />
-            </MemberOnly>
-          }
-        />
-        <Route
-          path='/acces'
-          element={
-            <UnauthenticatedOnly redirectTo='/'>
-              <AccountPage />
-            </UnauthenticatedOnly>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/panier' element={<CartPage />} />
+      <Route path='/epreuves' element={<OlympicEventsPage />} />
+      <Route
+        path='/acces/ouverture'
+        element={
+          <UnauthenticatedOnly redirectTo='/'>
+            <ActivationPage />
+          </UnauthenticatedOnly>
+        }
+      />
+      <Route
+        path='/acces/break'
+        element={
+          <UnauthenticatedOnly redirectTo='/'>
+            <ForgotPasswordPage />
+          </UnauthenticatedOnly>
+        }
+      />
+      <Route
+        path='/acces/reprise'
+        element={
+          <UnauthenticatedOnly redirectTo='/'>
+            <ResetPasswordConfirmPage />
+          </UnauthenticatedOnly>
+        }
+      />
+      <Route
+        path='/loge'
+        element={
+          <MemberOnly redirectTo='/acces'>
+            <AccountPage />
+          </MemberOnly>
+        }
+      />
+      <Route
+        path='/acces'
+        element={
+          <UnauthenticatedOnly redirectTo='/'>
+            <AccountPage />
+          </UnauthenticatedOnly>
+        }
+      />
+    </Routes>
   );
 };
 
